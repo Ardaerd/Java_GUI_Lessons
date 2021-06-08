0b336ba23544a0c74b515871f4210211098fc1ab
Ardaerd <arderd@outlook.com>
2021-06-08 15:45:56 +0300

    How can we create several identical frames

--- src/GUI_WithSeveralIdenticalFrame/AbsouluteLayoutWindow.java
+++ src/GUI_WithSeveralIdenticalFrame/AbsouluteLayoutWindow.java
@@ -10,22 +10,22 @@ public class AbsouluteLayoutWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
 
-        JPanel mainPanel = new JPanel();
-        frame.add(mainPanel);
-        mainPanel.setLayout(null);
-
-        JButton button = new JButton("Button");
-        button.setBounds(10,10,200,60);
-        mainPanel.add(button);
-
-        JTextField field = new JTextField("My size is manually set...");
-        field.setBounds(10,90,90,40);
-        mainPanel.add(field);
-
-        JLabel label = new JLabel("I am a label with a border.");
-        label.setBounds(50,120,200,80);
-        mainPanel.add(label);
-        label.setBorder(new EtchedBorder());
+//        JPanel mainPanel = new JPanel();
+//        frame.add(mainPanel);
+//        mainPanel.setLayout(null);
+//
+//        JButton button = new JButton("Button");
+//        button.setBounds(10,10,200,60);
+//        mainPanel.add(button);
+//
+//        JTextField field = new JTextField("My size is manually set...");
+//        field.setBounds(10,90,90,40);
+//        mainPanel.add(field);
+//
+//        JLabel label = new JLabel("I am a label with a border.");
+//        label.setBounds(50,120,200,80);
+//        mainPanel.add(label);
+//        label.setBorder(new EtchedBorder());
 
         frame.setVisible(true);
     }
