2acc843ca743a4a133d8164bbca76d94e9f07b84
Ardaerd <arderd@outlook.com>
2021-06-08 16:24:19 +0300

    several frames are created

--- src/GUI_WithSeveralIdenticalFrame/AbsoluteLayoutWindow.java
+++ src/GUI_WithSeveralIdenticalFrame/AbsoluteLayoutWindow.java
@@ -7,12 +7,18 @@ import java.awt.*;
 public class AbsoluteLayoutWindow {
     public static void main(String[] args) {
 
-        JFrame frame = new MyWindow();
-        frame.setTitle("Windows 1");
-        frame.setLocation(0,0);
+        for(int i = 0; i < 5; i++) {
+            JFrame frame = new MyWindow();
+            frame.setTitle("Window " + (i+1));
+            frame.setLocation(i*100,0);
+        }
 
-        JFrame frame2 = new MyWindow();
-        frame.setTitle("Window 2");
-        frame.setLocation(400,0);
+//        JFrame frame = new MyWindow();
+//        frame.setTitle("Windows 1");
+//        frame.setLocation(0,0);
+//
+//        JFrame frame2 = new MyWindow();
+//        frame.setTitle("Window 2");
+//        frame.setLocation(400,0);
     }
 }
