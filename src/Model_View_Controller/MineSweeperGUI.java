3eefe88102055444e0a5021c6d64e635b932db05
Ardaerd <arderd@outlook.com>
2021-06-08 23:38:37 +0300

    view is added

---
+++ src/Model_View_Controller/MineSweeperGUI.java
@@ -0,0 +1,19 @@
+package Model_View_Controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MineSweeperGUI extends JPanel {
+
+    public MineSweeperGUI (int numRows, int numCols, int numMines) {
+
+        setLayout(new GridLayout(numRows,numCols));
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                JButton button = new JButton();
+                add(button);
+            }
+        }
+    }
+}
