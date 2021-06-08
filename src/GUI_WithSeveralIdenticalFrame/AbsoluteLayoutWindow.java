f960cf777fb83f0c933f24aac6d44419e18fd543
Ardaerd <arderd@outlook.com>
2021-06-08 16:13:14 +0300

    Several frames are created

--- src/GUI_WithSeveralIdenticalFrame/AbsoluteLayoutWindow.java
+++ src/GUI_WithSeveralIdenticalFrame/AbsoluteLayoutWindow.java
@@ -6,30 +6,13 @@ import java.awt.*;
 
 public class AbsoluteLayoutWindow {
     public static void main(String[] args) {
+
         JFrame frame = new MyWindow();
+        frame.setTitle("Windows 1");
         frame.setLocation(0,0);
 
-//        JFrame frame = new JFrame();
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(400,400);
-
-//        JPanel mainPanel = new JPanel();
-//        frame.add(mainPanel);
-//        mainPanel.setLayout(null);
-//
-//        JButton button = new JButton("Button");
-//        button.setBounds(10,10,200,60);
-//        mainPanel.add(button);
-//
-//        JTextField field = new JTextField("My size is manually set...");
-//        field.setBounds(10,90,90,40);
-//        mainPanel.add(field);
-//
-//        JLabel label = new JLabel("I am a label with a border.");
-//        label.setBounds(50,120,200,80);
-//        mainPanel.add(label);
-//        label.setBorder(new EtchedBorder());
-
-//        frame.setVisible(true);
+        JFrame frame2 = new MyWindow();
+        frame.setTitle("Window 2");
+        frame.setLocation(400,0);
     }
 }
